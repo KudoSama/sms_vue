a150bef8f868858918a8e0c99d03b2dabc74ed4b
KudoSama <1226398122@qq.com>
2023-03-12 23:45:19 +0800

    优化非登录界面且未登录时跳转至登录界面功能，与后端进行配套

--- src/tools/http/http.js
+++ src/tools/http/http.js
@@ -45,31 +45,29 @@ function checkCode (res, errMsg) {
         Message.error('服务器连接错误！')
         break
       case 70005:
-        Message.error('您需要重新登录！')
+        // 不处于登录界面且未登录，显示完信息后跳转至登录界面
+        if (window.location.href !== window.location.origin + '/#/' &&
+          window.location.href !== window.location.origin + '/#/login' &&
+          window.location.href !== window.location.origin + '/#/login/' &&
+          window.location.href !== window.location.origin + '/' &&
+          window.location.href !== window.location.origin + '/login' &&
+          window.location.href !== window.location.origin + '/login/' &&
+          window.location.href !== window.location.origin) {
+          Vue.prototype.$message({
+            message: res.data.message,
+            type: 'error',
+            duration: 1500,
+            onClose: () => {
+              window.location.replace('/')
+            }
+          })
+        }
         break
       default:
-        if (res.data.message === '未登录，请重新登陆') {
-          // 不处于登录界面且未登录，显示完信息后跳转至登录界面
-          if (window.location.href !== window.location.origin + '/#/' &&
-            window.location.href !== window.location.origin + '/#/login' &&
-            window.location.href !== window.location.origin + '/' &&
-            window.location.href !== window.location.origin + '/login' &&
-            window.location.href !== window.location.origin) {
-            Vue.prototype.$message({
-              message: res.data.message,
-              type: 'error',
-              duration: 1500,
-              onClose: () => {
-                window.location.replace('/')
-              }
-            })
-          }
-        } else {
-          errMsg ? Message.error(errMsg) : Message.error(res.data.message || '未知异常')
-        }
+        errMsg ? Message.error(errMsg) : Message.error(res.data.message || '未知异常')
     }
     this.$axios.post('/api/user/refresh')
-    this.$router.push('/')
+    window.location.replace('/')
     this.$router.go(0)
   }
   return res.data
